3e5fdf4de825566af0f3a1e5ff9722942bef302a
Dariush Novin <dariushnovin@gmail.com>
2023-03-25 04:43:31 -0700

    Create Server and Config Application

--- app/server.js
+++ app/server.js
@@ -1,5 +1,3 @@
-//const express = require("express");
-
 module.exports = class Application {
     #express = require("express");
     #app = this.#express();
@@ -9,50 +7,57 @@ module.exports = class Application {
         this.createServer(PORT)
         this.createRoutes()
         this.errorHandler()
-    }
+    };
     configApplication(){
         const path = require("path");
         this.#app.use(this.#express.json());
         this.#app.use(this.#express.urlencoded({extended:true}));
         this.#app.use(this.#express.static(path.join(__dirname,"..","public")));
-    }
+    };
     createServer(PORT){
         const http = require("http");
         const server = http.createServer(this.#app)
         server.listen(PORT,()=>{
             console.log(`Server Run On http://localhost:${PORT}`)
         })
-    }
+    };
     configDatabase(DB_URL){
         const mongoose = require("mongoose");
-        mongoose.connect(DB_URL,(error)=>{
-            if(error) throw error
-            return console.log("Connect to DB successfully");
+        mongoose.set('strictQuery',true)
+        mongoose.connection
+        .on('error',(err)=>{
+            console.log("---DATABASE CONNECTION ERROR \n",err.message)
         })
-    }
+        .on('open',(err)=>{
+            console.log("CONNECTED TO MONGO DB")
+        })
+        return mongoose.connect(DB_URL);
+    };
     errorHandler(){
         this.#app.use((req,res,next)=>{
             return res.status(404).json({
                 status: 404,
-                sucess: false,
+                success: false,
                 message:"404 NOT FOUND"
-            });
+            })
+             
         });
-        this.#app((error,req,res,next)=>{
+        this.#app.use((error,req,res,next)=>{
             const status = error?.status || 500;
             const message = error?.message || "Internal Server Error";
             return res.status(status).json({
                 status,
-                sucess: false,
+                success: false,
                 message
             })
         })
-    }
+    };
     createRoutes(){
         this.#app.get("/",(req,res,next)=>{
-            return res.json({
+             return res.json({
                 message: "this is a new Express application"
             })
         })
-    }
+    };
 }
+
